93fdd428ac54d94183a75ae434bbaab6b51dcf70
jefersonwork <jefersoncosta336@gmail.com>
2023-04-21 14:48:12 -0300

    solid principles

---
+++ src/services/register.ts
@@ -0,0 +1,37 @@
+import { hash } from "bcryptjs"
+import { prisma } from "@/lib/prisma"
+import { PrismaUsersRepository } from "@/repositories/prisma-users-repository"
+
+interface RegisterServiceData {
+  name: string;
+  email: string;
+  password: string;
+}
+
+class RegisterService {
+  async execute({ 
+    name, 
+    email, 
+    password 
+  }: RegisterServiceData) {
+    const password_hash = await hash(password, 6)
+  
+    const userWithSameEmail = await prisma.user.findUnique({
+      where: {
+        email,
+      }
+    })
+  
+    if (userWithSameEmail) {
+      throw new Error('E-mail already exists.')
+    }
+  
+    const prismaUsersRepository = new PrismaUsersRepository()
+    
+    await prismaUsersRepository.create({ 
+      name, 
+      email, 
+      password_hash
+    })
+  }
+}
